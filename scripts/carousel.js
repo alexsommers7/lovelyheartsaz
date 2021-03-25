500ed6bb289bba8123fee13f2cbac4d64be18857
alexsommers7 <asommers22@hotmail.com>
2021-03-25 00:21:42 -0700

    updating mobile slider

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -15,71 +15,61 @@ const textElements = document.querySelectorAll(".carousel__text");
 
 let carouselItems = [];
 for (let i = 0; i < textElements.length; i++) {
-    carouselItems.push([
-        imgElements[i].src,
-        textElements[i].textContent
-    ]);
-};
+  carouselItems.push([imgElements[i].src, textElements[i].textContent]);
+}
 
 // create a dot for each slide. Store all in array
-buttons.forEach(button => {
-    let newContainer = document.createElement("div");
-    newContainer.classList.add("carousel__dot");
-    dotContainer.appendChild(newContainer);
+buttons.forEach((button) => {
+  let newContainer = document.createElement("div");
+  newContainer.classList.add("carousel__dot");
+  dotContainer.appendChild(newContainer);
 });
 const dots = document.querySelectorAll(".carousel__dot");
 
-// initialize counter var. Start in middle if on mobile
+// initialize counter variable
 let position = 0;
-if (screen.width <= 900) {
-    position = 2;
-}
 
 // slider functionality
 function carousel(n) {
+  // if at last item, reset to first. if at first item, go to last
+  if (n >= numButtons) n = 0;
+  else if (n < 0) n = numButtons - 1;
 
-    // if at last item, reset to first. if at first item, go to last
-    if (n >= numButtons) n = 0;
-    else if (n < 0) n = (numButtons - 1);
+  // calculate width of carousel item plus margins (for mobile)
+  let buttonWidth = document.querySelector(".service").getBoundingClientRect().width + 40;
 
-    // calculate width of carousel item plus margins (for mobile)
-    let buttonWidth = document.querySelector(".service:not(.active)").getBoundingClientRect().width + 40;
+  // if on mobile, move slide track
+  if (screen.width <= 900) {
+    buttonTrack.style.transform = `translateX(-${buttonWidth * n}px)`;
+  }
 
-    // if on mobile, move slide track
-    // because we are starting with position = 2 on mobile, we need to account for the + and - translates above/below the middle point
-    if (screen.width <= 900) {
-        if (n === 0) buttonTrack.style.transform = `translateX(${buttonWidth * (n + 2)}px)`
-        else if (n < 2) buttonTrack.style.transform = `translateX(${buttonWidth * n}px)`
-        else buttonTrack.style.transform = `translateX(-${buttonWidth * (n - 2)}px)`
-    }
+  // remove active class for all buttons & dots
+  for (let i = 0; i < numButtons; i++) {
+    buttons[i].classList.remove("active");
+    dots[i].classList.remove("active");
+  }
 
-    // remove active class for all buttons & dots
-    for (let i = 0; i < numButtons; i++) {
-        buttons[i].classList.remove("active");
-        dots[i].classList.remove("active");
-    }
+  // set active class to current slide & dot
+  buttons[n].classList.add("active");
+  dots[n].classList.add("active");
 
-    // set active class to current slide & dot
-    buttons[n].classList.add("active");
-    dots[n].classList.add("active");
+  // show content at n index
+  carouselImage.src = carouselItems[n][0];
+  carouselText.textContent = carouselItems[n][1];
 
-    // show content at n index
-    carouselImage.src = carouselItems[n][0];
-    carouselText.textContent = carouselItems[n][1];
+  // add animation class
+  carouselImage.classList.add("carouselImageFade");
+  carouselText.classList.add("carouselTextFade");
 
-    // add animation class
-    carouselImage.classList.add('carouselImageFade');
-    carouselText.classList.add('carouselTextFade');
+  // remove animation class once animation finishes
+  setTimeout(function () {
+    carouselImage.classList.remove("carouselImageFade");
+    carouselText.classList.remove("carouselTextFade");
+  }, 500);
 
-    // remove animation class once animation finishes
-    setTimeout(function () {
-        carouselImage.classList.remove('carouselImageFade');
-        carouselText.classList.remove('carouselTextFade');
-    }, 500);
-
-    // update and return counter variable
-    return position = n;
-};
+  // update and return counter variable
+  return (position = n);
+}
 
 // call function on page load to initalize
 carousel(position);
@@ -89,37 +79,44 @@ let autoplay = setInterval(() => carousel(position + 1), 5000);
 
 // call carousel function and kill autoplay on click of button/dot/arrow
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-        carousel(i);
-        clearInterval(autoplay);
-    })
-    dots[i].addEventListener("click", function () {
-        carousel(i);
-        clearInterval(autoplay);
-    })
-    if (i < arrows.length) {
-        arrows[i].addEventListener("click", function () {
-            clearInterval(autoplay);
-        })
-    }
-};
-
+  buttons[i].addEventListener("click", function () {
+    carousel(i);
+    clearInterval(autoplay);
+  });
+  dots[i].addEventListener("click", function () {
+    carousel(i);
+    clearInterval(autoplay);
+  });
+  if (i < arrows.length) {
+    arrows[i].addEventListener("click", function () {
+      clearInterval(autoplay);
+    });
+  }
+}
 
 // swiping functionality on touchscreen
 let touchstartX = 0;
 let touchendX = 0;
 
 function handleGesture() {
-    if (touchendX < touchstartX) carousel(position + 1);
-    if (touchendX > touchstartX) carousel(position - 1);
+  if (touchendX < touchstartX) carousel(position + 1);
+  if (touchendX > touchstartX) carousel(position - 1);
 }
 
-buttonTrack.addEventListener('touchstart', e => {
+buttonTrack.addEventListener(
+  "touchstart",
+  (e) => {
     touchstartX = e.changedTouches[0].screenX;
-}, { passive: true });
+  },
+  { passive: true }
+);
 
-buttonTrack.addEventListener('touchend', e => {
+buttonTrack.addEventListener(
+  "touchend",
+  (e) => {
     touchendX = e.changedTouches[0].screenX;
     handleGesture();
     clearInterval(autoplay);
-}, { passive: true });
+  },
+  { passive: true }
+);
