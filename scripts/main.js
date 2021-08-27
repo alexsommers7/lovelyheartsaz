76441a91cfc826c5bd74aacb1c1a91ee651ebcdc
alexsommers7 <asommers22@hotmail.com>
2021-08-26 22:41:19 -0700

    footer content update

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,37 @@
 // INTERSECTION OBSERVER API - FADE ON SCROLL
 const observerOptions = {
   root: null, // Null = use viewport as root
-  rootMargin: '0px', // Margin if desired for root
-  threshold: 0.25 // Percentage of visibility needed on target element to run callback function
+  rootMargin: "0px", // Margin if desired for root
+  threshold: 0.25, // Percentage of visibility needed on target element to run callback function
 };
 
 function observerCallback(entries, observer) {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
       // Fade in from top
-      if (entry.target.classList.contains('fadeFromTop')) {
-        entry.target.classList.replace('fadeFromTop', 'fadeFromTopVisible');
+      if (entry.target.classList.contains("fadeFromTop")) {
+        entry.target.classList.replace("fadeFromTop", "fadeFromTopVisible");
       }
       // Fade in from bottom
-      else if (entry.target.classList.contains('fadeFromBottom')) {
-        entry.target.classList.replace('fadeFromBottom', 'fadeFromBottomVisible');
+      else if (entry.target.classList.contains("fadeFromBottom")) {
+        entry.target.classList.replace("fadeFromBottom", "fadeFromBottomVisible");
       }
       // Fade in from left
-      else if (entry.target.classList.contains('fadeFromLeft')) {
-        entry.target.classList.replace('fadeFromLeft', 'fadeFromLeftVisible');
+      else if (entry.target.classList.contains("fadeFromLeft")) {
+        entry.target.classList.replace("fadeFromLeft", "fadeFromLeftVisible");
       }
       // Fade in from right
-      else if (entry.target.classList.contains('fadeFromRight')) {
-        entry.target.classList.replace('fadeFromRight', 'fadeFromRightVisible');
+      else if (entry.target.classList.contains("fadeFromRight")) {
+        entry.target.classList.replace("fadeFromRight", "fadeFromRightVisible");
       }
     }
   });
-};
+}
 
-const fadeElms = document.querySelectorAll('.fade');
+const fadeElms = document.querySelectorAll(".fade");
 
 const observer = new IntersectionObserver(observerCallback, observerOptions);
-fadeElms.forEach(el => observer.observe(el));
+fadeElms.forEach((el) => observer.observe(el));
+
+// FOOTER YEAR
+document.getElementById("footer__year").innerText = new Date().getFullYear();
